4ce34006173ddc3b272f4bc2a313889dc2a9121b
Andrey Tkachuk <45736309+AndreyTkachuk@users.noreply.github.com>
2020-06-22 23:40:15 +0300

    SpringSecurityMysql

---
+++ src/main/java/com/company/service/UserService.java
@@ -0,0 +1,10 @@
+package com.company.service;
+
+import com.company.model.User;
+
+public interface UserService {
+	public User findUserByEmail(String email);
+	public void saveUser(User user);
+	
+	
+}
